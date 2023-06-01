d7618e3294db00027ff360fe0713b5b3fe95a12c
Asadullah <asadullah@destring.com>
2023-06-01 18:07:35 +0500

    code standard processing start

--- plugin.php
+++ plugin.php
@@ -9,7 +9,7 @@ Version: 1.0
 
 defined( 'ABSPATH' ) or die( 'No script kiddies please!' );
 define( 'SKELETON_PLUGIN_FILE', __FILE__ );
-define( 'SKELETON_PLUGIN_DIR_PATH', plugin_dir_path(SKELETON_PLUGIN_FILE ));
+define( 'SKELETON_PLUGIN_DIR_PATH', plugin_dir_path( SKELETON_PLUGIN_FILE ));
 define( 'SKELETON_PLUGIN_DIR_URL', plugin_dir_url( SKELETON_PLUGIN_FILE ));
 require_once __DIR__."/vendor/autoload.php";
 use SkeletonPlugin\App;
